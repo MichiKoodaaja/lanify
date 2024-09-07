4b6e34c7ab705d5860659de5205eb58a44886d43
Michi Koodaaja <p31516@edu.sasky.fi>
2024-09-07 09:25:02 +0300

    lomaketiedon tarkistaminen

--- src/view/lisaa_tili.php
+++ src/view/lisaa_tili.php
@@ -5,15 +5,18 @@
 <form action="" method="POST">
   <div>
     <label for="nimi">Nimi:</label>
-    <input id="nimi" type="text" name="nimi">
+    <input id="nimi" type="text" name="nimi" value="<?= getValue($formdata,'nimi') ?>">
+    <div class="error"><span><?= getValue($error,'nimi'); ?></span></div>
   </div>
   <div>
     <label for="email">Sähköposti:</label>
-    <input id="email" type="email" name="email">
+    <input id="email" type="email" name="email" value="<?= getValue($formdata,'email') ?>">
+    <div class="error"><?= getValue($error,'email'); ?></div>
   </div>
   <div>
     <label for="discord">Discord-tunnus:</label>
-    <input id="discord" type="text" name="discord">
+    <input id="discord" type="text" name="discord" value="<?= getValue($formdata,'discord')?>">
+    <div class="error"><?= getValue($error,'discord'); ?></div>
   </div>
   <div>
     <label for="salasana1">Salasana:</label>
@@ -22,6 +25,7 @@
   <div>
     <label for="salasana2">Salasana uudelleen:</label>
     <input id="salasana2" type="password" name="salasana2">
+    <div class="error"><?= getValue($error,'salasana'); ?></div>
   </div>
   <div>
     <input type="submit" name="laheta" value="Luo tili">
