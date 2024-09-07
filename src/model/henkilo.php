93f86833c34d1ad90b2b84eaaf3693a6e62a4fb0
Michi Koodaaja <p31516@edu.sasky.fi>
2024-09-07 09:29:16 +0300

    sähköpostiosoitteen varauksen tarkistus

--- src/model/henkilo.php
+++ src/model/henkilo.php
@@ -6,5 +6,8 @@
     DB::run('INSERT INTO henkilo (nimi, email, discord, salasana) VALUE  (?,?,?,?);',[$nimi,$email,$discord,$salasana]);
     return DB::lastInsertId();
   }
+  function haeHenkiloSahkopostilla($email) {
+    return DB::run('SELECT * FROM henkilo WHERE email = ?;', [$email])->fetchAll();
+  }
 
 ?>
